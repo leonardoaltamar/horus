2ecd347868da9927c64ef2eb5be9275e064ad219
Michael Rodriguez <73129506+maicolrg11@users.noreply.github.com>
2021-02-01 15:18:46 -0500

    add: menu, not-responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,27 @@ import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NbButtonModule, NbCardModule, NbLayoutModule, NbSidebarModule, NbThemeModule, NbMenuModule} from '@nebular/theme';
 import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './shared/layout/layout.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+//PrimeNG
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     MenuComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
-    NbThemeModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
-    NbLayoutModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
-    NbCardModule,
-    NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
-    NbButtonModule,
-    NbMenuModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    OverlayPanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NbMenuItem } from '@nebular/theme';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements OnInit {
+  isMenuVisible: boolean;
 
   constructor() { }
 
