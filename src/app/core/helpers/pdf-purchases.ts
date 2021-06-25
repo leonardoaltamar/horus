7b7b9a8925d9e497c64ebf5b625c3f5edc0a4ccb
leonardoaltamar <leonardoaltamar12@gmail.com>
2021-06-24 22:42:07 -0500

    update pdf-purchases

--- src/app/core/helpers/pdf-purchases.ts
+++ src/app/core/helpers/pdf-purchases.ts
@@ -1,12 +1,11 @@
-import { PdfMakeWrapper, Txt, Table, Img, Columns, Cell, Toc, TocItem, Canvas, Polyline} from 'pdfmake-wrapper';
-import { ITable, IToc } from 'pdfmake-wrapper/lib/interfaces';
+import { PdfMakeWrapper, Txt, Table, Columns, Toc} from 'pdfmake-wrapper';
 import pdfFonts from "pdfmake/build/vfs_fonts";
 
 
 const extractDetails = (details)=>{
   let dataRow = [];
   details.forEach(item => {
-    dataRow.push([item.article.name, item.quantity,item.unitValue, (item.total*item.unitValue)])
+    dataRow.push([item.article.name, item.quantity,item.unitValue, (item.quantity * item.unitValue)])
   });
   return dataRow;
 }
@@ -35,18 +34,18 @@ export const generatePdfPurchases= (modelPayment)=>{
 
   //datos del proveedor
   pdf.add(new Columns(
-    [new Columns([ new Txt('Nombre completo').bold().end, new Txt(`${modelPayment.supplier?.person?.name} ${modelPayment.supplier?.person?.surname} ${modelPayment?.supplier?.person?.secondSurname} `).end ]).columnGap(-25).end,
-     new Columns([ new Txt('Fecha de pago').bold().end, new Txt(`${modelPayment.dateInvoice}`).end ]).columnGap(-25).end
+    [new Columns([ new Txt('Nombre completo:').bold().end, new Txt(`${modelPayment.supplier?.person?.name} ${modelPayment.supplier?.person?.surname} ${modelPayment?.supplier?.person?.secondSurname} `).end ]).columnGap(-40).end,
+     new Columns([ new Txt('Fecha de la compra:').bold().end, new Txt(`${modelPayment.dateInvoice}`).end ]).columnGap(-75).end
     ]).columnGap(60).margin([0,0,0,5]).end);
 
   pdf.add(new Columns(
-    [new Columns([ new Txt('cedula').bold().end, new Txt(`${modelPayment?.supplier?.person?.documentNumber}`).end ]).columnGap(-25).end,
-      new Columns([ new Txt('Dirección').bold().end, new Txt(`${modelPayment?.address || ''}`).end ]).columnGap(-25).end
+    [new Columns([ new Txt('Documento de identidad:').bold().end, new Txt(`${modelPayment?.supplier?.person?.documentNumber}`).end ]).columnGap(3).end,
+      new Columns([ new Txt('Dirección:').bold().end, new Txt(`${modelPayment?.address || ''}`).end ]).columnGap(-25).end
     ]).margin([0,0,0,5]).columnGap(60).end);
 
   pdf.add(new Columns(
-    [new Columns([ new Txt('Empresa ').bold().end, new Txt(`${modelPayment?.supplier?.bussiness?.name}`).end ]).columnGap(-25).end,
-      new Columns([ new Txt('Tipo de pago').bold().end, new Txt(`${modelPayment?.typePayment?.description || ''}`).end ]).columnGap(-25).end
+    [new Columns([ new Txt('Empresa: ').bold().end, new Txt(`${modelPayment?.supplier?.bussiness?.name || ''}`).end ]).columnGap(-25).end,
+      new Columns([ new Txt('Tipo de pago:').bold().end, new Txt(`${modelPayment?.typePayment?.description || ''}`).end ]).columnGap(-25).end
     ]).margin([0,0,0,5]).columnGap(60).end);
 
   pdf.add(new Table([[new Txt("Detalle de la Compra").bold().end]]).widths([500]).margin([0,5,0,0]).end);
@@ -56,15 +55,16 @@ export const generatePdfPurchases= (modelPayment)=>{
       [new Txt("Artículos").bold().end, new Txt("cantidad").bold().end, new Txt("vlr.unidad").bold().end,
       new Txt("vlr.total").bold().end ],
       ...extractDetails(modelPayment.details)
-  ]).widths([140,100,100, 100]).layout({
+  ]).widths([150,110,110, 103]).layout({
     fillColor: (rowIndex: number, node: any, columnIndex: number)=>{
         return rowIndex === 0 ? '#cccccc' : '';
     }
-  }).margin([10,10,0,10]).end)
+  }).margin([0,-1,0,10]).end)
 
   //tabla de totales
   pdf.add(new Table([
-    [ new Txt('Sub-total: ').bold().end , `$ ${modelPayment.total}`],
+    [ new Txt('IVA: ').bold().end , `$ ${modelPayment.totalLien}`],
+    [ new Txt('Sub-total: ').bold().end , `$ ${modelPayment.subTotal}`],
     [ new Txt('Descuentos: ').bold().end,'$ 0'],
     [new Txt('otros: ').bold().end,'$ 0'],
     [new Txt('valor Total: ').bold().end,`$ ${modelPayment.total}`],
